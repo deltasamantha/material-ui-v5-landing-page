8675c08475c611260fe1461ba7ba713d8e64c535
Samantha Withanage <samantha@frostdigital.se>
2021-12-26 12:45:25 +0530

    Setup tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders a land like", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("A land like");
   expect(linkElement).toBeInTheDocument();
 });
